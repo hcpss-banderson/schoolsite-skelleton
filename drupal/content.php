3fb456761b11931977fc88086919dc637dbda4af
Brendan Anderson <brendan_anderson@hcpss.org>
2021-12-14 11:34:09 -0500

    Add assets

--- drupal/content.php
+++ drupal/content.php
@@ -1,5 +1,10 @@
 <?php
 
+use Symfony\Component\DomCrawler\Crawler;
+use Goutte\Client;
+
+require_once './vendor/autoload.php';
+
 /**
  * A class for querying the HCPSS school api.
  */
@@ -75,13 +80,33 @@ $query = new SchoolQuery();
 $schools = $query->getSchools();
 
 foreach ($schools as $acronym => $school) {
-  if (!in_array($acronym, ['cles'])) {
-    continue;
-  }
   
-  echo "$acronym\n";
+  $client = new Client();
+  $crawler = $client->request('GET', "https://{$acronym}.hcpss.org");
+  
+//   $icon = $crawler->filter('img.school-logo')->first()->attr('src');
+//   $file = file_get_contents($icon);
+//   file_put_contents("/var/www/drupal/web/themes/custom/equity/images/{$acronym}.png", $file);
+  
+  $colors = ":root {\n";
+  $colors .= "  --main-primary-color: {$school['primary_color']};\n";
+  $colors .= "  --main-secondary-color: {$school['secondary_color']};\n";
+  $colors .= "  --main-highlight-color: {$school['highlight_color']};\n";
+  $colors .= "  --banner: url('https://www.hcpss.org/f/mrb/base/hs-theme/images/banner-{$acronym}.jpg');\n";
+  $colors .= "  --mascot: url('/themes/custom/equity/images/$acronym.png');\n";
+  $colors .= "}\n";
+  
+  file_put_contents("/var/www/drupal/web/themes/custom/equity/css/schools/$acronym.css", $colors);
+
+  $library = "school_{$acronym}:\n  css:\n    theme:\n      css/schools/{$acronym}.css: {}\n";
+  
+  //file_put_contents('/var/www/drupal/web/themes/custom/equity/equity.libraries.yml', $library, FILE_APPEND);
+  
+
+  /*
   shell_exec("drush --root=/var/www/drupal si -y --account-pass=admin --existing-config");
   shell_exec("drush --root=/var/www/drupal php:script --script-path=/var/www/drupal/drush/scripts scrape-departments -- {$acronym}");
   shell_exec("drush --root=/var/www/drupal content-snapshot:export -y --snapshot-path=/schools/{$acronym}/content");
+  */
 }
 
